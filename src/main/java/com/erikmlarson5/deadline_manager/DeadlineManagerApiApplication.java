743e1a8ae02f0f62388de226f6cfac6636faa85f
Erik Larson <erikmlarson5@gmail.com>
2025-06-25 16:07:27 -0400

    init commit

---
+++ src/main/java/com/erikmlarson5/deadline_manager/DeadlineManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.erikmlarson5.deadline_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadlineManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadlineManagerApiApplication.class, args);
+	}
+
+}
