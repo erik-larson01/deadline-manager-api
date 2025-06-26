61890885e0cd91a0fa8b88eab2dfed1fd26c6689
Erik Larson <erikmlarson5@gmail.com>
2025-06-26 12:54:57 -0400

    project entity and status enum

--- src/main/java/com/erikmlarson5/deadlinemanager/entity/Project.java
+++ src/main/java/com/erikmlarson5/deadlinemanager/entity/Project.java
@@ -1,4 +1,144 @@
 package com.erikmlarson5.deadlinemanager.entity;
 
+import com.erikmlarson5.deadlinemanager.utils.Status;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long projectId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+    private String course;
+
+    @Column(nullable = false)
+    private LocalDate dueDate;
+
+    @Column(nullable = false)
+    private float weight;
+
+    @Column(nullable = false)
+    private int difficulty;
+
+    private float priority = 0.0f;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+
+    public Project() {
+
+    }
+
+    public Project(String title, String description, String course, LocalDate dueDate,
+                   float weight, int difficulty, Status status) {
+        this.title = title;
+        this.description = description;
+        this.course = course;
+        this.dueDate = dueDate;
+        this.weight = weight;
+        this.difficulty = difficulty;
+        this.status = status;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public float getPriority() {
+        return priority;
+    }
+
+    protected void setPriority(float priority) {
+        this.priority = priority;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
---
+++ src/main/java/com/erikmlarson5/deadlinemanager/utils/Status.java
@@ -0,0 +1,9 @@
+package com.erikmlarson5.deadlinemanager.utils;
+
+public enum Status {
+    NOT_STARTED,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
+
