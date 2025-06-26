63d531c52c89d04c53ac9e899d585f2fe16dd75a
Erik Larson <erikmlarson5@gmail.com>
2025-06-26 16:05:42 -0400

    project and task repository base implementation

--- src/main/java/com/erikmlarson5/deadlinemanager/repository/ProjectRepository.java
+++ src/main/java/com/erikmlarson5/deadlinemanager/repository/ProjectRepository.java
@@ -1,4 +1,15 @@
 package com.erikmlarson5.deadlinemanager.repository;
 
-public class ProjectRepository {
+import com.erikmlarson5.deadlinemanager.entity.Project;
+import com.erikmlarson5.deadlinemanager.utils.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findByStatus(Status status);
+
+    List<Project> findAllByOrderByPriorityDesc();
 }
--- src/main/java/com/erikmlarson5/deadlinemanager/repository/TaskRepository.java
+++ src/main/java/com/erikmlarson5/deadlinemanager/repository/TaskRepository.java
@@ -1,4 +1,18 @@
 package com.erikmlarson5.deadlinemanager.repository;
 
-public class TaskRepository {
+import com.erikmlarson5.deadlinemanager.entity.Project;
+import com.erikmlarson5.deadlinemanager.entity.Task;
+import com.erikmlarson5.deadlinemanager.utils.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByStatus(Status status);
+
+    List<Task> findByProject_ProjectId(Long projectId);
+
+    List<Task> findAllByOrderByPriorityDesc();
 }
