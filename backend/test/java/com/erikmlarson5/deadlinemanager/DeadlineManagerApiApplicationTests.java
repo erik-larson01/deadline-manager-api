71d223b448b60691a1d3ea2f62586f2ee17e2efa
Erik Larson <erikmlarson5@gmail.com>
2026-03-17 16:32:42 -0500

    added base api test

---
+++ backend/test/java/com/erikmlarson5/deadlinemanager/DeadlineManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erikmlarson5.deadlinemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadlineManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
